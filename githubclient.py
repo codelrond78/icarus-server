dffa2a161465b6dc3bff6fa6b4dbcaaeb654ca16
Lord Codelrond <codelrond@protonmail.com>
2021-12-19 15:32:41 +0100

    added pygit2

--- githubclient.py
+++ githubclient.py
@@ -4,4 +4,4 @@ from ptoken import token
 g = Github(token)
 
 for repo in g.get_user().get_repos():
-    print(repo.name)
+    print(str(repo))
---
+++ gitinfo.py
@@ -0,0 +1,10 @@
+import pygit2
+
+repo = pygit2.Repository('.')
+
+#print(repo, dir(repo))
+#print(repo.branches)
+for br in repo.branches:
+    print(br)
+current = repo.head.shorthand
+print(current)
