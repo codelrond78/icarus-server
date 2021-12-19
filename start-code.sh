82c8ddba7794ee72a98a35b33841a00120ef6b9f
Lord Codelrond <codelrond@protonmail.com>
2021-12-19 12:34:22 +0100

    expose 5000 port

--- start-code.sh
+++ start-code.sh
@@ -1,2 +1,2 @@
 #docker run --rm -it --name code-icarus-back --env PASSWORD=123 -v settingsv2:/root/.local/share/code-server -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app/hello -v ~/.ssh:/root/.ssh:ro -p 8081:8080 py38
-docker run --rm -it --name code-icarus-back --env PASSWORD=123 -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app/hello -v ~/.ssh:/root/.ssh:ro -p 8081:8080 py38
+docker run --rm -it --name code-icarus-back --env PASSWORD=123 -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app/hello -v ~/.ssh:/root/.ssh:ro -p 8081:8080 -p 8888:5000 py38
