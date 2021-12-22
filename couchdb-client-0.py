609848636fdf2e8bf5a892b565ec36aab9c4e8dc
Lord Codelrond <codelrond@protonmail.com>
2021-12-22 12:43:25 +0100

    wip

--- couchdb-client-0.py
+++ couchdb-client-0.py
@@ -1,5 +1,26 @@
 import pycouchdb
 server = pycouchdb.Server("http://admin:123@couchdb:5984/")
 
-print(server.info()['version'])
+#db = server.create("foo2")
+db = server.database("foo2")
+
+#doc = db.save({"name": "bar"})
+
+"""
+_doc = {
+    "_id": "_design/testing",
+    "views": {
+        "names": {
+            "map": "function(doc) { emit(doc, 1); }",
+            "reduce": "function(k, v) { return  sum(v); }",
+        }
+    }
+}
+
+db.save(_doc)
+"""
+
+result = list(db.query("testing/names", group='true'))
+print(result)
+#print(server.info()['version'])
 #
