c4ff8e120814934c1df63d01e5834007bad801ea
Lord Codelrond <codelrond@protonmail.com>
2021-12-27 13:35:29 +0100

    wip

--- commands.js
+++ commands.js
@@ -4,19 +4,27 @@ const { spawn } = require("child_process");
     
 const WORKSPACES_PATH = '/workspaces';
 
+function directoriesInDIrectory(pth){
+    return fs.readdirSync(pth, { withFileTypes: true })
+    .filter((item) => item.isDirectory())
+    .map((item) => item.name);
+}
+
 function updateWorkspace(name, specification, raw){
 
     myPath = path.join(WORKSPACES_PATH, name);
-    //fs.mkdirSync(myPath);
     fs.writeFileSync(path.join(myPath, 'docker-compose.yaml'), raw, err => {
         if (err) {
           console.error(err)
           return
         }
     });
-    //for(let name of specification.services){
-    //    fs.mkdirSync(path.join(wpath, name));
-    //}
+    const dirs = directoriesInDIrectory(myPath);
+    for(let name of specification.services){
+        if(!dirs.includes(name)) {
+            fs.mkdirSync(path.join(myPath, name));
+        }   
+    }
 }
 
 
@@ -31,7 +39,7 @@ function createWorkspace(name, specification, raw){
         }
     });
     for(let name of specification.services){
-        fs.mkdirSync(path.join(wpath, name));
+        fs.mkdirSync(path.join(myPath, name));
     }
 }
 
