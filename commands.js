6499b41996e312ccbe63e9637b44af1efa9e06cc
codelrond <codelrond@protonmail.com>
2022-02-06 16:05:43 +0000

    try catch in commands

--- commands.js
+++ commands.js
@@ -2,19 +2,23 @@ const { spawn } = require("child_process");
 const { logInputLine, logOutputLine} = require("./logdatabase");
     
 async function command(cmd, name, specification, db){
-    cmd_ = spawn(cmd, [], { env: { YAML: specification, NAME: name }})
+    try{
+        let cmd_ = spawn(cmd, [], { env: { YAML: specification, NAME: name }})
 
-    await logInputLine(cmd + ' ' + name, db)
-    cmd_.stdout.on("data", async data => {
-        data = data.toString();
-        console.log(`stdout: ${data}`);
-        await logOutputLine(data, db)
-    }); 
-    cmd_.stderr.on("data", async data => {
-        data = data.toString();
-        console.log(`stderr: ${data}`);
-        await logOutputLine(data, db)
-    });
+        await logInputLine(cmd + ' ' + name, db)
+        cmd_.stdout.on("data", async data => {
+            data = data.toString();
+            console.log(`stdout: ${data}`);
+            await logOutputLine(data, db)
+        }); 
+        cmd_.stderr.on("data", async data => {
+            data = data.toString();
+            console.log(`stderr: ${data}`);
+            await logOutputLine(data, db)
+        });
+    }catch(err){
+        console.log('error at command', err)
+    }
 }
 
 async function run(name, specification, db){
